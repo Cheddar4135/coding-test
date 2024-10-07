9ace362307abd016e4d6d886719cf61de057eae3
Cheddar4135 <101963492+Cheddar4135@users.noreply.github.com>
2024-10-07 21:46:53 +0900

    미해결

---
+++ PART 01/004 - 피보나치수열.py
@@ -0,0 +1,7 @@
+"""
+[leetcode] 509. Fibonacci Number
+https://leetcode.com/problems/fibonacci-number/description/
+
+#O(logn) #Recursive
+"""
+def search(self, nums: List[int], target: int) -> int:
