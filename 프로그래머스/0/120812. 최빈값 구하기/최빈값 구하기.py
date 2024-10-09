23521b86104fa16f10c8296865a5384a236b538e
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-10-09 16:13:01 +0900

    #딕셔너리 마스터 #enumerate() #Counter()는 개꿀이다

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,5 +1,5 @@
 def solution(array):
-    """최빈값 구하기
+    """딕셔너리를 이용한 풀이
     array : 정수 배열
     answer : 최빈값 return, 최빈값 여러개일시 -1 return
     
@@ -30,4 +30,40 @@ def solution(array):
         return -1
     else:
         return keys[max_index[0]]
-            
+
+def solution(array):
+    """count를 활용한 다른 사람의 풀이
+    list의 count함수는 특정 원소가 list에 몇개 있는지 반환한다.
+    문제의 array길이는 1000개로 제한되어있다.
+ 
+    array : [1,1,2,2,2,3,3,3,3]
+    idx : [0,2,3,4,0,0,0,0,0,0,,0,0,0,0,0,0,0,0,0,0,0,...]
+    idx.count(4) = 1 -> return 3
+    
+    워우..이해하기도 힘드네.. 어떻게 한거지.
+    함수하나 또 배웠다. list.index(4) : 4라는 원소의 인덱스를 반환
+    """
+    idx = [0] * 1001
+    for i in array:
+        idx[i] +=1
+    if idx.count(max(idx)) >1:
+        return -1
+    return idx.index(max(idx))
+
+from collections import Counter
+def solution(array):
+    """counter를 이용한 풀이
+    Counter(["hi", "hey", "hi", "hi", "hello", "hey"])
+    >>> Counter({'hi': 3, 'hey': 2, 'hello': 1}) 
+    Counter("hello")
+    >>> Counter({'h': 1, 'e': 1, 'l': 2, 'o': 1}) 카운터객체 반환한다
+    Counter 클래스는 딕셔너리를 확장하고 있기 때문에, 딕셔너리에서 제공하는 API를 그대로 다 사용가능
+    아, 내가 딕셔너리로 한 짓을 Counter로 쉽게 할 수 있구나.
+    """
+    a = Count(array).most_common(2)
+    if len(a) == 1: #예외처리: array의 원소가 다 같은 수였을때 혹은 하나였을때의 경우 list out of range 방지
+        return a[0][0]
+    if a[0][1] == a[1][1]:
+        return -1
+    return a[0][0]
+    
