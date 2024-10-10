95c7a0a585d8380cdb0a27b7167d84d461d57d58
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-10-10 13:38:48 +0900

    #range(start, end, step) #np.arange(start, end, step)

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,12 +1,10 @@
 import numpy as np
 def solution(n):
-    """n 이하의 홀수 리스트를 반환한다.
-    9   [1,3,5,7,9]
-    2   [1]
-    4   [1,3]
-    
-    1. 가장 쉬운 방법은 arange아닐까.
-    """
-    
     answer = np.arange(1,n+1,2).tolist()
     return answer
+
+def solution(n):
+    return [x for x in range(n+1) if x % 2]
+
+def solution(n):
+    return [x for x in range(1,n+1,2)]
