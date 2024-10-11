7e76e55306bb082881079665c484f8ca439962e1
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-10-11 10:42:15 +0900

    #divmode()

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,3 +1,11 @@
 def solution(money):
     price = 5500
     return [money//price, money%price]
+
+def solution(money):
+    price = 5500
+    return list(divmod(money,price))
+
+"""파이썬 내장함수 divmod()를 이용하면 몫과 나머지를 한번에 구할 수 있다.
+계산결과를 (몫, 나머지) 튜플로 반환해준다.
+"""
