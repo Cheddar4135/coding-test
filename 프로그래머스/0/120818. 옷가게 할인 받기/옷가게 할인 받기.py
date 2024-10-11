0484401f611574b9faa3310aaeb1eb4a2b72f0ba
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-10-11 10:34:36 +0900

    쉽지만 확장성 좋게 푸는 방법이 있었군

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -7,3 +7,10 @@ def solution(price):
         return int(price*0.95)
     else:
         return price
+
+#딕셔너리 이용한 다른사람의 신박한 풀이. 확실히 이러면 코드 확장성이 좋겠군.
+def solution(price):
+    discount_rates = {500000: 0.8, 300000: 0.9, 100000: 0.95, 0: 1}
+    for discount_price, discount_rate in discount_rates.items():
+        if price >= discount_price:
+            return int(price * discount_rate)
