4c822c2aeac0f99c8daeeb0639465092241592bc
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-10-13 16:17:59 +0900

    Update 짝수 홀수 개수.py

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -6,3 +6,18 @@ def solution(num_list):
         else:
             odd+=1
     return [even, odd]
+
+def solution(num_list):
+    """나머지를 인덱스로 활용한 센스있는 풀이
+    """
+    answer = [0,0] #짝수, 홀수 순으로 반환해야함
+    for num in num_list:
+        answer[num%2]+=1
+    return answer
+
+    """만약 홀수, 짝수 순으로 반환해야했다면
+    """
+    answer = [0,0] #홀수, 짝수 순으로 반환
+    for num in num_list:
+        answer[(num+1)%2]+=1
+    return answer
