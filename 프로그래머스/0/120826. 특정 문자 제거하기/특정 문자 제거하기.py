1c714c3c3ead19aedbf5582586a82c7352eba6d6
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-10-16 12:14:19 +0900

    #''.join() #string.replace(letter,'')

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,2 +1,8 @@
 def solution(my_string, letter):
     return ''.join(my_string.split(letter))
+
+def solution(my_string, letter):
+    return ''.join(c for c in my_string if c != letter)
+
+def solution(my_string, letter):
+    return my_string.replace(letter, '')
