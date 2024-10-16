6409833af9242229dc913fe68aa8e9312f947b04
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-10-16 12:24:59 +0900

    # 쉽지만 센스있는 풀이도 있다

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -9,3 +9,17 @@ def solution(angle):
         return 4
     else:
         print("angle범위가 초과되었습니다.")
+
+
+def solution(angle):
+    """코드분석
+    angle//90 : 예각이라면 0, 평각이라면 2, 둔각이나 직각은 1
+    angle%90 : 평각,직각이라면 0, 둔각이나 예각은 0보다큰 값
+
+    예각 : 0 + 1
+    직각: 2 + 0
+    둔각: 2 + 1
+    평각: 4 + 0
+    """
+    answer = (angle // 90) * 2 + (angle % 90 > 0) * 1
+    return answer
