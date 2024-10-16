4ca4904bc31eb166da65b5edacb3129cd89c481f
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-10-16 12:46:12 +0900

    #등차수열 합공식 #range(2,n+1,2)

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
@@ -5,5 +5,11 @@ def solution(n):
     등차수열 합공식: num*(2a+(num-1)d)/2 => num * (4 +(num-1)*2)/2
 
     """
-    
+    num = n//2
+    return num * (4 +(num-1)*2)/2
+
+def solution(n):
+    """
+    range는 이미 iterable객체이기 때문에 바로 sum함수에 넣을 수 있다.
+    """
     return sum(range(2,n+1,2))
