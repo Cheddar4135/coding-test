859f0bf39eda5a2244783e7bac216c4eedfb415c
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-10-18 09:52:45 +0900

    수정용이성도 챙기고 코드 간단성도 챙기고

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -1,5 +1,5 @@
 def solution(hp):
-    """
+    """공격력 수정 용이성이 좋은 코드
     이 코드는 개미군단이 언제나 사냥감의 체력에 딱 맞는 병력을 데리고 나갈 수 있다는 가정,
     즉 일개미의 공격력이 늘 1이라는 가정하에만 정답을 내놓을 수있다.
     """
@@ -9,3 +9,17 @@ def solution(hp):
     num3 = (hp%attack['장군개미'])%attack['병정개미']
     
     return num1+num2+num3
+
+def solution(hp):
+    """간단히 하면 그냥 이 코드
+    """
+    return hp // 5 + (hp % 5 // 3) + ((hp % 5) % 3)
+
+def solution(hp):
+    """이게 best 방법인듯, 수정용이성도 좋고 코드도 간단하고
+    """
+    answer = 0
+    for ant in [5,3,1]:
+        count,hp = divmode(hp,ant)
+        answer+=count
+    return answer
