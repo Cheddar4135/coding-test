eecf7bb94385bf9fa16d42942188f37fd6401715
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-10-18 10:36:34 +0900

    factorial 구현하기 : 재귀 vs for문

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -1,6 +1,30 @@
 from math import factorial as fact
+import math
+
 def solution(balls, share):
-    """
+    """치사방법1. math모듈쓰기
     nCr : n!/(n-r)!r!
     """
     return fact(balls)/fact(balls-share)/fact(share)
+
+def solution(balls,share):
+    """더 치사한 방법2: math.comb쓰기
+    """
+    return math.comb(balls,share)
+    
+def fact(n):
+    """factorial 재귀적으로 구현하기
+    문제: 몇가지 케이스에서 runtime 에러 발생
+    원인:  스택 깊이 초과 Stack Overflow Error
+    해결 : 그냥 for문으로 풀 수 있는 건 for문으로 풀자.
+    """
+    if n==1:
+        return 1
+    else:
+        return n*fact(n-1)
+
+def fact(n):
+    result = 1
+    for i in range(1,n+1):
+        result*=i
+    return result
