da98ebd534e5627d1db81314510b50f4eed1592f
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-10-22 14:55:48 +0900

    라이브러리 의존한게 약간 반성됨. 직접 구현하는 것도 꼭 해봐야함

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -2,3 +2,12 @@ import numpy as np
 def solution(num_list, n):
     a = np.array(num_list)
     return a.reshape(-1,n).tolist()
+
+def solution(num_list, n):
+    """다른사람풀이
+    약간 반성되네..
+    """
+    answer = []
+    for i in range(0, len(num_list), n):
+        answer.append(num_list[i:i+n])
+    return answer
