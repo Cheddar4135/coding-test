1045ad494dbd37572b33eac65661e10fb6049861
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-10-22 14:41:56 +0900

    나도 이렇게 센스있게 풀고싶다

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -12,3 +12,10 @@ def solution(dot):
     else:
         print("잘못된 입력")
         return -1
+
+def solution(dot):
+    """센스있는 풀이
+    x가 양수: 1,4사분면 y가양수:1사분면
+    x가 음수: 2,3사분면 y가양수:2사분면
+    """
+    return [[1, 4], [2, 3]][dot[0] < 0][dot[1] < 0]
