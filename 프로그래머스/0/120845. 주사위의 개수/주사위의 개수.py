1b3eca74016a0e9682a05d2fe3a7e0a53deab3ae
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-11-05 15:01:20 +0900

    [level 0] Title: 주사위의 개수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.py
@@ -0,0 +1,14 @@
+def solution(box, n):
+    """
+    > 함수 정보:
+        box : [가로길이, 세로길이,높이길이]
+        n : 주사위 모서리 길이
+        return (int): 상자에 들어갈 수 있는 주사위 최대 개수
+        
+    > main idea: 박스의 각 모서리의 길이를 주사위 모서리 길이로 나눈 몫만큼 들어갈 수 있다.
+    """
+    sum = 1
+    box = [(boxlen//n) for boxlen in box]
+    for i in box:
+        sum*=i
+    return sum
