9e76ad30a12c86786474a315c9fa885b54398472
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-11-05 15:25:51 +0900

    # n의 약수의 중간은 n**0.5

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -25,3 +25,12 @@ def solution(n):
     for num in range(1,n+1):
         sum+=int(isComposite(num))
     return sum
+
+def soultion(n):
+    res = 0
+    for i in range(4,n+1):
+        for j in range(2,int(i**0.5)+1):
+            if i%j==0:
+                res+=1
+                break
+    return res
