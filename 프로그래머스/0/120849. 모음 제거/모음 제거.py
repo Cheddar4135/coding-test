e0db5877ef778cbf00693f76a70f16085c8f7417
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-11-08 18:42:02 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    ans = my_string.replace('a','')
+    ans = ans.replace('e','')
+    ans = ans.replace('i','')
+    ans = ans.replace('o','')
+    ans = ans.replace('u','')
+    return ans
