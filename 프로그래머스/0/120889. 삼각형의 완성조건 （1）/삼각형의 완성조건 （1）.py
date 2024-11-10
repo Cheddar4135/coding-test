26eded490caad7f4f54b74ffcdeaa1e5df8300ed
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-11-10 10:30:12 +0900

    sort()는 O(nlogn) , max()는 O(n)

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
@@ -1,3 +1,12 @@
 def solution(sides):
     sides=sorted(sides)
     return 1 if sides[2]<(sides[0]+sides[1]) else 2
+
+def solution(sides):
+    sides.sort()
+    return 1 if sides[2]<(sides[0]+sides[1]) else 2
+
+"""
+Q. 파이썬 sort()는 O(nlogn)의 복잡도 필요한데, 안쓰는게 낫지 않아?
+A. 어차피 sides는 [변길이1,변길이2,변길이3] 3개의 원소만을 담는 리스트야. 어떤 경우든 O(1)이지. 걍 써.
+""" 
