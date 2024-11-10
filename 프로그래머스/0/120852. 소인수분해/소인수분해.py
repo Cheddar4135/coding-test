2dad97e73acdc65da5176302499d96166f5a3f59
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-11-10 11:29:58 +0900

    [level 0] Title: 소인수분해, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -1,10 +1,14 @@
 def solution(n):
     ans = []
+    k = 2
     while n>1:
-        for k in range(2,n+1):
-            if n%k==0:
-                ans.append(k)
-                n//=k
-                break
+        if n%k==0:
+            ans.append(k)
+            n//=k
+        elif k >int(n**0.5):
+            ans.append(n)
+            break
+        else:
+            k+=1
     return sorted(list(set(ans)))
     
