589019bf68efebd84b09a5f8364e639e6a304654
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-11-19 13:58:09 +0900

    make it simple #list[start,end,step] #range(start,end,step)

--- 프로그래머스/0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -4,3 +4,9 @@ def solution(cipher, code):
         ans += cipher[code-1]
         cipher=cipher[code:]
     return ans
+
+def solution(cipher, code):
+    return cipher[code-1::code]
+
+def solution(cipher,code):
+    return ''.join(cipher[i] for i in range(code-1,len(cipher),code))
