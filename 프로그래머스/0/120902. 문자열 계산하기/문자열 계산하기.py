c68c5fe4d6d6aa262fe9738ec51db798071d8c01
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-11-22 08:38:02 +0900

    유익했다 발전을 위해 다른 사람 풀이를 봐야하는 이유

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -7,3 +7,26 @@ def solution(my_string):
         elif item == "-":
             ans-=int(my_list[i+1])
     return ans
+
+
+"""
+발전 : 연산자는 무조건 짝수번째 요소에 있으니까 꼭 리스트의 모든 요소를 순회할 필요가 없다.
+"""
+def solution(my_string):
+    my_list = my_string.split(' ')
+    ans=int(my_list[0])
+    for i in range(1,len(my_list),2):
+        if item == "+":
+            ans+=int(my_list[i+1])
+        elif item == "-":
+            ans-=int(my_list[i+1])
+    return ans
+
+
+"""
+프로그래머스 1등 풀이
+1. replace(' - ',' + -' ) "3 + 4 + 5 - 5" => "3 + 4 + 5 + -5" 
+2. split(' + ')하고 각 요소 정수화해서
+"""
+def solution(my_string):
+    return sum([int(i) for i in my_string.replace(' - ',' + -').split(' + ')])
