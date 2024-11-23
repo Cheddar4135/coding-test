9bee0b5c1ca5db744846ae60ead18d96bbd4b972
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-11-23 19:18:04 +0900

    # dict = { i : 1 for i in my_list}  딕셔너리 컴프리헨션

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,6 +1,5 @@
+#그냥 반복문 쓰는 것보다 시간 복잡도 절약 가능
 def solution(s1, s2):
     dict = {i:1 for i in s1}
-    answer = sum(1 for j in s2 if j in dict)
-    
-    
+    answer = sum(1 for j in s2 if j in dict) 
     return answer
