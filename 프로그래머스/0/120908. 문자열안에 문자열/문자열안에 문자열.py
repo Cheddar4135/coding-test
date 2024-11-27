63367615583a0fda89a265dba601b24e2bd50bf4
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-11-28 08:51:05 +0900

    # 문자열안에 문자열 : "abc" in "abcde"?

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -1,2 +1,8 @@
 def solution(str1, str2):
     return 2 if str1.replace(str2,"")==str1 else 1
+
+def solution(str1, str2):
+    return 1 if str2 in str1 else 2
+
+def solution(str1,str2):
+    return 1 + int(str2 not in str1)
