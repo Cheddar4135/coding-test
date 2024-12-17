1eb900887eb892f59ff61dd35ca202db89330b20
최다현 <101963492+Cheddar4135@users.noreply.github.com>
2024-12-17 10:59:06 +0900

    #너무 쉬운 문제라고 그냥 넘기면 안되는 이유 #비트 연산 효과적 풀이 배울 수 있음

--- 프로그래머스/0/120910. 세균 증식/세균 증식.py
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.py
@@ -1,2 +1,12 @@
 def solution(n, t):
     return n*2**t
+
+
+def solution(n,t):
+    #n의 비트를 왼쪽으로 t번 옮김. n * 2^t의미
+    return n << t 
+
+"""why it can be possible?
+ 비트 연산은 말그대로 비트를 움직이는 거다.
+ 비트를 왼쪽 또는 오른쪽으로 이동시키면 2를 곱하거나 나눈 효과가 나타난다.
+"""
